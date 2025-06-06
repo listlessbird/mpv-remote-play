bfbcc770976935898fc79af75cad6274c7fe769f
listlessbird <124798751+listlessbird@users.noreply.github.com>
2025-06-06 17:28:21 +0530

    feat(app): add settings tab with blur effect and update dependencies

--- expo-app/src/app/(tabs)/_layout.tsx
+++ expo-app/src/app/(tabs)/_layout.tsx
@@ -1,9 +1,49 @@
+import { colors, fontSize } from "@/lib/constants/tokens"
+import { BlurView } from "expo-blur"
 import { Tabs } from "expo-router"
+import { StyleSheet } from "react-native"
+import { FontAwesome } from "@expo/vector-icons"
 
 export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarLabelStyle: {
+          fontSize: fontSize.xs,
+          fontWeight: "500",
+        },
+        headerShown: false,
+        tabBarStyle: {
+          position: "absolute",
+          borderTopRightRadius: 20,
+          borderTopLeftRadius: 20,
+          borderTopWidth: 0,
+          paddingTop: 8,
+        },
+        tabBarBackground: () => (
+          <BlurView
+            intensity={95}
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              overflow: "hidden",
+              borderTopRightRadius: 20,
+              borderTopLeftRadius: 20,
+            }}
+          />
+        ),
+      }}
+    >
       <Tabs.Screen name="(home)" />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="gear" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   )
 }
---
+++ expo-app/src/app/(tabs)/settings/index.tsx
@@ -0,0 +1,10 @@
+import { defaultStyles } from "@/styles"
+import { View, Text, StyleSheet } from "react-native"
+
+export default function SettingsScreen() {
+  return (
+    <View style={defaultStyles.container}>
+      <Text style={defaultStyles.text}>Settings Screen</Text>
+    </View>
+  )
+}
